051372221791b9c6d2ca0b4955ad41fb460f3309
davronoff <ilyosbekdavronov009@gmail.com>
2022-11-22 15:05:04 +0500

    Third commit

--- app/src/main/java/com/rtm/git/MainActivity.kt
+++ app/src/main/java/com/rtm/git/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("This is my second commit")
+        println("This is my third commit")
     }
 }
