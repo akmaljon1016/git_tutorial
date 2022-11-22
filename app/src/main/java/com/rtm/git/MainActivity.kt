36f8274879b04384384e4365791413ac5fea29b1
davronoff <ilyosbekdavronov009@gmail.com>
2022-11-22 15:04:12 +0500

    Second commit

---
+++ app/src/main/java/com/rtm/git/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rtm.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("This is my second commit")
+    }
+}
